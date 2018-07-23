b8bfec18f43b47385441bcb7de47002f4e3270cc
Nikola <41384642+NT996@users.noreply.github.com>
2018-07-23 17:00:41 +0200

    Models

---
+++ Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFProject.Model
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Autor { get; set; }
+        public decimal Price { get; set; }
+        public Publisher Publisher { get; set; }
+        public int PublisherID { get; set; }
+        public int GenreID { get; set; }
+        public virtual ICollection<Genre> Genres { get; set; }
+
+
+    }
+}
