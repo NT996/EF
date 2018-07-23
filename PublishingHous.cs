89e304de7acc74d8457d37339a355853c92a9e18
Nikola <41384642+NT996@users.noreply.github.com>
2018-07-23 17:02:37 +0200

    Add files via upload

---
+++ PublishingHous.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using CFProject.Model;
+
+namespace CFProject
+{
+    class PublishingHous : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+
+        public PublishingHous()
+            :base("name=DefaultConnection")
+        { }
+    }
+}
